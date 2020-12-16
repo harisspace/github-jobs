197d3258c55d8da345560f7efd2d4c4a5eac52f8
haris akbar <harisakbar04@gmail.com>
2020-12-16 09:33:31 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useFetchJobs from './useFetchJobs'
+import { Container } from 'react-bootstrap'
 
 function App() {
+  const [params, setParamas] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error } = useFetchJobs(params, page)
+
   return (
-    <div className="App">
-      
-    </div>
+    <Container>
+      { loading && <h1>loading...</h1>}
+      { error && <h1>error, try refreshing</h1>}
+      <h1>{ jobs.length }</h1>
+    </Container>
   );
 }
 
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,5 +1,50 @@
-// export default useFetchJobs(params, page) {
-//     return {
-//        loading : true
-//     }
-// }
+import axios from 'axios'
+import { useEffect, useReducer } from 'react'
+
+const BASE_URL = "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
+
+const ACTIONS = {
+    MAKE_REQUEST: 'make_request',
+    GET_DATA: 'get_data',
+    ERROR: 'error'
+}
+
+function reducer(state, action) {
+    switch(action.type) {
+        case ACTIONS.MAKE_REQUEST:
+            return { loading: true, jobs: []}
+        case ACTIONS.GET_DATA:
+            return { ...state, loading: false, jobs: action.payload.jobs}
+        case ACTIONS.ERROR:
+            return { ...state, loading: false, jobs: action.payload.error}
+        default: 
+            return state;
+    }
+}
+
+export default function useFetchJobs(params, page) {
+    const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true})
+
+    useEffect(() => {
+       // cancel request with cancelToken
+       const cancelToken = axios.CancelToken;
+       const source = cancelToken.source(); 
+
+        dispatch({ type: ACTIONS.MAKE_REQUEST })
+        axios.get(BASE_URL, {
+            params: { markdown: true, page, ...params}
+        }).then(res => {
+            dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data }})
+        }).catch(e => {
+            if (axios.isCancel(e)) return;
+            dispatch({ type: ACTIONS.ERROR, payload: { error: e }})
+        })
+
+    })
+
+    return {
+        jobs: [],
+        loading: false,
+        error: false
+    }
+}
